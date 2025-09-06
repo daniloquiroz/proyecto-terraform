cc6cb1a27a984fa19f7a7ca942d072149c098fce
notifi <notificaciones.gitlab@dcsas.com.co>
2025-09-06 16:04:58 -0500

    ajuste nuevo

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "nginx_server_qa" {
-  source        = "./modules/nginx_server"
+  source        = "./modules/nginx_servers"
   instance_type = var.instance_type
   server_name   = var.server_name
   environment   = var.environment
   region        = var.region
-  ssh_public_key = var.ssh_public_key  # si tu módulo lo espera (recomendado)
+  ssh_public_key = var.ssh_public_key
 }
